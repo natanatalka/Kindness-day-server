bb21a695450a9d48551ff128128868024c554ccb
Natalia Savchak <nsavchak@softjourn.com>
2018-02-11 17:03:06 +0200

    Update sendMail.js

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -16,7 +16,7 @@ let sendMail = (ctx, user) => {
         }
 
         $ = cheerio.load(html.toString());
-        $('#link').attr('href', `http://kindness.testing.softjourn.if.ua/api/receiver/` + user.uniqueId);
+        $('#link').attr('href', `${config.url}/receiver/` + user.uniqueId);
 
         let data = {
             to: user.email,
